40db7c04cd879b690e53f5b3c11b1cec4e2d1f43
Lucas <luccaslp99@gmail.com>
2023-09-20 19:17:28 -0300

    create docker compose

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,27 @@
 import fastify from "fastify"
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: "lucas",
+    email: "teste@teste.com",
+  },
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
