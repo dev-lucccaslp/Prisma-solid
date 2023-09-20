f7b6efbdbaadae37328dae3a54151e8eaa0c236a
Lucas <luccaslp99@gmail.com>
2023-09-20 19:31:19 -0300

    ajusted bugs in versions

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import fastify from "fastify"
-<<<<<<< HEAD
+
 import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
@@ -12,31 +12,3 @@ prisma.user.create({
     email: "teste@teste.com",
   },
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-import { PrismaClient } from "@prisma/client"
-
-export const app = fastify()
-
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    name:'Lucas',
-    email: 'teste@teste.com'
-  }
-})
->>>>>>> 6278e62e985a4622d968846609832ec76ca0a8a2
